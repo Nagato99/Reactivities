68ac4924ac9f3747a56206119dc1a48a0cfca3a7
Nagato <manandu20@gmail.com>
2021-12-18 02:04:36 +0200

    CancellationToken

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -24,7 +25,7 @@ namespace Application.Activities
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.ToListAsync();
+                return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
     }
