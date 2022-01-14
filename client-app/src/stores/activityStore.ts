5c4d431e799ad924c6ee5d50780ab93fe4649eaf
Nagato <manandu20@gmail.com>
2022-01-14 22:07:20 +0200

    Create Activity using MobX

--- client-app/src/stores/activityStore.ts
+++ client-app/src/stores/activityStore.ts
@@ -1,6 +1,7 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
+import { v4 as uuid } from "uuid";
 
 export default class ActivityStore {
     activities: Activity[] = [];
@@ -17,15 +18,15 @@ export default class ActivityStore {
         this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
-                activities.forEach(activity => {
-                    activity.date = activity.date.split('T')[0];
-                    this.activities.push(activity);
-                })
-                this.setLoadingInitial(false);
+            activities.forEach(activity => {
+                activity.date = activity.date.split('T')[0];
+                this.activities.push(activity);
+            })
+            this.setLoadingInitial(false);
         } catch (error) {
             console.log(error);
             this.setLoadingInitial(false);
-            
+
         }
     }
 
@@ -33,7 +34,7 @@ export default class ActivityStore {
         this.loadingInitial = state;
     }
 
-    selectActivity =(id: string) => {
+    selectActivity = (id: string) => {
         this.selectedActivity = this.activities.find(a => a.id === id);
     }
 
@@ -46,7 +47,45 @@ export default class ActivityStore {
         this.editMode = true;
     }
 
-    closeForm =() => {
+    closeForm = () => {
         this.editMode = false;
+    }
+
+    createActivity = async (activity: Activity) => {
+        this.loading = true;
+        activity.id = uuid();
+        try {
+            await agent.Activities.create(activity);
+            runInAction(() => {
+                this.activities.push(activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    }
+
+    updateActivity = async (activity: Activity) => {
+        this.loading = true;
+        try {
+            await agent.Activities.update(activity);
+            runInAction(() => {
+                this.activities = [...this.activities.filter(a => a.id !== activity.id), activity];
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
     }
 }
