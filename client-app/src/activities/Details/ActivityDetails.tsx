df0d5628bf838d6ad18fb9b632fd47c7378f35f7
Nagato <manandu20@gmail.com>
2022-01-15 20:28:44 +0200

    Getting an individual activity

--- client-app/src/activities/Details/ActivityDetails.tsx
+++ client-app/src/activities/Details/ActivityDetails.tsx
@@ -6,8 +6,8 @@ import { useStore } from "../../stores/store";
 
 export default function ActivityDetails() {
 
-    const {activityStore} = useStore();
-    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+    const { activityStore } = useStore();
+    const { selectedActivity: activity } = activityStore;
 
     if (!activity) return <LoadingComponent />;
 
@@ -25,8 +25,8 @@ export default function ActivityDetails() {
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths='2'>
-                    <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
-                    <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel' />
+                    <Button basic color='blue' content='Edit' />
+                    <Button basic color='grey' content='Cancel' />
                 </Button.Group>
             </Card.Content>
         </Card>
